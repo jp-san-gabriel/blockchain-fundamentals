13f29c5897e6feeeeddee8cfa195db32428e066f
JEAN PAUL T. SAN GABRIEL <jp.san.gabriel01@gmail.com>
2024-08-19 22:23:28 +0800

    09 - Write test for Block

--- block.js
+++ block.js
@@ -7,11 +7,4 @@ class Block {
     }
 }
 
-const block1 = new Block({
-    timestamp: '18/11/2021', 
-    lastHash: 'foo-lastHash', 
-    hash: 'foo-hash', 
-    data: 'foo-data'
-});
-
-console.log('block1', block1);
+module.exports = Block;
