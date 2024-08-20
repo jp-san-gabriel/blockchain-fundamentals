60cf0737ae88333d8ee5a042f87b1ad6027c9a70
JEAN PAUL T. SAN GABRIEL <jp.san.gabriel01@gmail.com>
2024-08-20 23:20:51 +0800

    20 - Implement Blockchain.isValidChain() static function

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,6 @@
 const Block = require('./block');
+const { GENESIS_DATA } = require('./config');
+const cryptoHash = require('./crypto-hash');
 
 class Blockchain {
     constructor() {
@@ -9,6 +11,23 @@ class Blockchain {
         const lastBlock = this.chain[this.chain.length - 1];
         const minedBlock = Block.mineBlock({ lastBlock, data });
         this.chain.push(minedBlock);
+    }
+
+    static isValidChain(chain) {
+        if (JSON.stringify(chain[0]) != JSON.stringify(Block.genesis()))
+            return false;
+        for (let i = 1; i < chain.length; i ++) {
+            const block = chain[i];
+            const actualLastHash = chain[i - 1].hash
+            const { timestamp, lastHash, hash, data } = block;
+            if (lastHash !== actualLastHash)
+                return false;
+            const validatedHash = cryptoHash(timestamp, lastHash, data);
+
+            if (hash != validatedHash) return false;
+        }
+
+        return true;
     }
 }
 
