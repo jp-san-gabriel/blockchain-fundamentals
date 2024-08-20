4626a5b8c0ba1549aa1a083bcd74727b65d17560
JEAN PAUL T. SAN GABRIEL <jp.san.gabriel01@gmail.com>
2024-08-20 18:31:18 +0800

    14 - Use cryptoHash in Block.mineBlock() function

--- block.js
+++ block.js
@@ -1,4 +1,5 @@
 const { GENESIS_DATA } = require('./config');
+const cryptoHash = require('./crypto-hash');
 
 class Block {
     constructor({ timestamp, lastHash, hash, data }) {
@@ -13,10 +14,14 @@ class Block {
     }
 
     static mineBlock({ lastBlock, data }) {
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+
         return new this({
-            lastHash: lastBlock.hash,
+            lastHash,
             data,
-            timestamp: Date.now()
+            timestamp, 
+            hash: cryptoHash(timestamp, lastHash, data)
         })
     }
 }
