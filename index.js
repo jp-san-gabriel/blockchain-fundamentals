3838cfbd56dbe3cbd05540de69444e287aa6ee8d
JEAN PAUL T. SAN GABRIEL <jp.san.gabriel01@gmail.com>
2024-08-23 14:30:31 +0800

    47 - Prevent root node from synchronizing its chain on startup

--- index.js
+++ index.js
@@ -50,5 +50,6 @@ const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
     console.log(`listening at localhost:${PORT}`); 
 
-    syncChains();
+    if (PORT != DEFAULT_PORT)
+        syncChains();
 });
