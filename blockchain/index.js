8caee9f224410ad5323cea12f5f93a7d5789bb80
JEAN PAUL T. SAN GABRIEL <jp.san.gabriel01@gmail.com>
2024-08-23 15:21:43 +0800

    51 - Organize code

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,6 +1,6 @@
 const Block = require('./block');
-const { GENESIS_DATA } = require('./config');
-const cryptoHash = require('./crypto-hash');
+const { GENESIS_DATA } = require('../config');
+const cryptoHash = require('../util/crypto-hash');
 
 class Blockchain {
     constructor() {
