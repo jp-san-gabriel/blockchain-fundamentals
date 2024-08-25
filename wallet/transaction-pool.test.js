8d64d9eb8e35ef9cdea8e323070f75a54fcf5b8f
JEAN PAUL T. SAN GABRIEL <jp.san.gabriel01@gmail.com>
2024-08-25 12:10:53 +0800

    70 - Update existing transactions for API requests

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -3,12 +3,14 @@ const Transaction = require('./transaction');
 const Wallet = require('./index');
 
 describe('TransactionPool', () => {
-    let transactionPool, transaction;
+    let transactionPool, transaction, senderWallet;
 
     beforeEach(() => {
         transactionPool = new TransactionPool();
+        senderWallet = new Wallet();
+
         transaction = new Transaction({
-            senderWallet: new Wallet(),
+            senderWallet,
             recipient: 'fake-recipient',
             amount: 50
         });
@@ -20,6 +22,18 @@ describe('TransactionPool', () => {
 
             expect(transactionPool.transactionMap[transaction.id])
                 .toBe(transaction);
+        });
+    });
+
+    describe('existingTransaction()', () => {
+        it('returns an existing transaction given an inputAddress', () => {
+            transactionPool.setTransaction(transaction);
+
+            expect(
+                transactionPool.existingTransaction({
+                    inputAddress: senderWallet.publicKey
+                })
+            ).toBe(transaction);
         });
     });
 });
