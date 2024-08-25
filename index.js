f165a855d611f01585662131dfc6cefd4b2d989f
JEAN PAUL T. SAN GABRIEL <jp.san.gabriel01@gmail.com>
2024-08-25 11:29:38 +0800

    68 - Add POST /api/transact endpoint

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const request = require('request');
 const Blockchain = require('./blockchain');
 const bodyParser = require('body-parser');
 const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
 
 const app = express();
 const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain });
 
 const DEFAULT_PORT = 3000;
@@ -25,6 +29,14 @@ app.post('/api/mine', (req, res) => {
     pubsub.broadcastChain();
 
     res.redirect('/api/blocks');
+});
+
+app.post('/api/transact', (req, res) => {
+    const { amount, recipient } = req.body;
+
+    const transaction = wallet.createTransaction({ recipient, amount });
+
+    transactionPool.setTransaction(transaction);
 });
 
 const syncChains = () => {
