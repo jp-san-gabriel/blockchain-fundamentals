24c6e2ce9156fcead34155bb97e562bfe4158954
JEAN PAUL T. SAN GABRIEL <jp.san.gabriel01@gmail.com>
2024-08-30 14:15:30 +0800

    121 - Create component for displaying transaction pool

--- client/src/components/ConductTransaction.js
+++ client/src/components/ConductTransaction.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FormGroup, FormControl } from 'react-bootstrap';
+import { FormGroup, FormControl, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 class ConductTransaction extends Component {
@@ -11,6 +11,19 @@ class ConductTransaction extends Component {
 
     updateAmount = event => {
         this.setState({ amount: Number(event.target.value) });
+    }
+
+    conductTransaction = () => {
+        const { recipient, amount } = this.state;
+
+        fetch('http://localhost:3000/api/transact', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ recipient, amount })
+        }).then(response => response.json())
+            .then((json => {
+                alert(json.message || json.type);
+            }));
     }
 
     render () {
@@ -34,6 +47,14 @@ class ConductTransaction extends Component {
                         onChange={this.updateAmount}
                     />
                 </FormGroup>
+                <div>
+                    <Button
+                        bsStyle="danger"
+                        onClick={this.conductTransaction}
+                    >
+                        Submit
+                    </Button>
+                </div>
             </div>
         )
     }
