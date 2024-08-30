f114a1f0a11ec8dd3d54b8e6944ffc0a60b790aa
JEAN PAUL T. SAN GABRIEL <jp.san.gabriel01@gmail.com>
2024-08-30 11:21:16 +0800

    118 - Separate home and blocks pages with react router

--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Block from './Block';
+import { Link } from 'react-router-dom';
 
 class Blocks extends Component {
     state = { blocks: [] }
@@ -15,6 +16,7 @@ class Blocks extends Component {
 
         return (
             <div>
+                <div><Link to='/'>Home</Link></div>
                 <h3>Blocks</h3>
                 {
                     blocks.map(block => (
